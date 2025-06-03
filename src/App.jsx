53bc06d6cf3ab55d40010247c8f46f3e30205bd8
Sam Turner <sam.turner@kokadjo.net>
2025-06-02 21:59:11 -0400

    started header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import Header from './components/Header.jsx'
+
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+
+}
 
 export default App;
